6df3883502612023bdffc3bb1668d3f3d75e80f0
salaesteve2 <salamanca.esteve@gmail.com>
2024-09-25 14:59:12 +0200

    comments update

--- main.py
+++ main.py
@@ -14,6 +14,8 @@ def tuple_state(state):
 def main():
     init()
     start_time = time.time()
+
+    # Gestion argumentos
     parser = argparse.ArgumentParser()
     parser.add_argument("map", help="Map puzzle's side.")
     parser.add_argument("mode", help="Heuristic function(Manhattan, Hamming, Costo Uniforme, Nilsson).")
@@ -39,14 +41,12 @@ def main():
         print(Fore.RED + "Error. The mode dont exist." + Style.RESET_ALL)
         sys.exit()
 
-    # Parsear mapa
+    # Abrir archivo
     lines = maps.leer_mapa(args.map)
 
-    # abrir archivo
+    # Parsear mapa
     matriz, size = maps.procesar_mapa(lines)
-
     matrix = [number for sublist in matriz for number in sublist]
-
     rango = all(0 <= num <= size * size - 1 for num in matrix)
 
     # gestion de posibles errores
@@ -61,11 +61,11 @@ def main():
         print(Fore.YELLOW + str(lines) + Style.RESET_ALL)
 
     #Matriz del mapa a ordenar
-
     print("Initial matrix: ")
     for lines in matriz:
         print(Fore.CYAN + str(lines) + Style.RESET_ALL)
 
+    # Calcular si es resoluble
     matrix_snale_aplanado = [number for sublist in matrix_snale for number in sublist]
     if not is_solvable.is_solvable(matrix, matrix_snale_aplanado, size):
         print('Solvable:' + Fore.RED + ' False' + Style.RESET_ALL)
@@ -79,7 +79,6 @@ def main():
         print('Visualizer:' + Fore.RED + ' False' + Style.RESET_ALL)
 
     # Aplicar algoritmo solucion
-
     visited = set()
     open_set = {}
     maps_list = []
@@ -87,12 +86,14 @@ def main():
     g_cost = {0: 0}
     h_cost = 0
 
-
+    # Gestionar greedy search
     if not args.greedy_search:
         g_cost = {tuple_state(matriz): 0}
         print('Greedy Search:' + Fore.RED +' False' + Style.RESET_ALL)
     else:
         print('Greedy Search:' + Fore.GREEN + ' True' + Style.RESET_ALL)
+
+    # Gestionar heuristica
     if mode == "Manhattan":
         print('Heuristic: ' + Fore.LIGHTMAGENTA_EX + 'Manhattan' + Style.RESET_ALL)
         h_cost = heuristics.distance_manhattan(size, matriz, matrix_snale)
@@ -109,11 +110,16 @@ def main():
 
     heapq.heappush(maps_list, (f_cost, matriz))
     open_set[tuple_state(matriz)] = f_cost
+
+
     spiral_sequence = heuristics.generate_spiral_sequence(size)
+
+    # Bucle principal
     while maps_list:
         _, estado_actual = heapq.heappop(maps_list)
         lista_actual = tuple_state(estado_actual)
 
+        # Datos a imprimir si hay solución
         if estado_actual == matrix_snale:
 
             print(Fore.GREEN +"Solution found" + Style.RESET_ALL)
@@ -134,17 +140,23 @@ def main():
         # generar movimientos
         for sucesores in path.generar_sucesores(size, estado_actual):
             lista_sucesores = tuple_state(sucesores)
+
+            # Matriz ya vista, se pasa a la siguiente
             if lista_sucesores in visited:
                 continue
 
+            # Gestionar greedy search
             if not args.greedy_search:
                 posibilidad_g = g_cost[lista_actual] + 1
             else:
                 posibilidad_g = 0
 
+            # Gestión si el costo es menor que la tupla anterior
             if lista_sucesores not in g_cost or posibilidad_g < g_cost[lista_sucesores]:
                 recorrido[lista_sucesores] = lista_actual
                 g_cost[lista_sucesores] = posibilidad_g
+
+                # Aplicar heuristica correspondiente
                 if mode == "Manhattan":
                     h_cost = heuristics.distance_manhattan(size, sucesores, matrix_snale)
                 elif mode == "Hamming":
@@ -154,16 +166,18 @@ def main():
                 elif mode == "Nilsson":
                     posibilidad_g = heuristics.nilsson(sucesores, spiral_sequence, size) * 3
                     h_cost = heuristics.distance_manhattan(size, sucesores, matrix_snale)
+
+                # Greedy search
                 if args.greedy_search:
                     posibilidad_g = 0
+
+                # Costo total
                 f_cost = posibilidad_g + h_cost
+
+                # Meter tupla si el cambio se queda
                 if lista_sucesores not in open_set or f_cost < open_set[lista_sucesores]:
                     open_set[lista_sucesores] = f_cost
                     heapq.heappush(maps_list, (f_cost, sucesores))
-    print(Fore.RED + "Sin solución" + Style.RESET_ALL)
-    end_time = time.time()
-    execution_time = end_time - start_time
-    print('Tiempo de ejecución: ' + Fore.GREEN + f"{execution_time:.5f}" + ' segundos' + Style.RESET_ALL)
 
 if __name__ == "__main__":
     main()
